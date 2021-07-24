3305d083d11c4020aea1777cafc3a118e3961e75
DaiYuanchuan <novel-web@novelweb.cn>
2021-07-24 18:58:19 +0800

    update index.js

--- index.js
+++ index.js
@@ -1,52 +1,58 @@
-const express = require("express");
-const multer = require("multer");
-const tf = require("@tensorflow/tfjs-node");
+const express = require('express')
+const multer = require('multer')
+const jpeg = require('jpeg-js')
+
+const tf = require('@tensorflow/tfjs-node')
 const nsfw = require("nsfwjs");
 
-const app = express();
-const storage = multer.memoryStorage();
-const upload = multer({ storage });
+const app = express()
+const upload = multer()
 
-app.use(express.json());
+let _model
 
-async function main() {
-  const model = await nsfw.load();
-  tf.enableProdMode();
+const convert = async (img) => {
+    // Decoded image in UInt8 Byte array
+    const image = await jpeg.decode(img, true)
 
-  app.post(
-    "/single/multipart-form",
-    upload.single("content"),
-    async (request, response) => {
-      const content = request.file;
+    const numChannels = 3
+    const numPixels = image.width * image.height
+    const values = new Int32Array(numPixels * numChannels)
 
-      const tfContent = await tf.node.decodeImage(content.buffer, 3);
-      const prediction = await model.classify(tfContent);
-      tfContent.dispose();
+    for (let i = 0; i < numPixels; i++)
+        for (let c = 0; c < numChannels; ++c)
+            values[i * numChannels + c] = image.data[i * 4 + c]
 
-      return response.json({ prediction });
-    }
-  );
-
-  app.post(
-    "/multiple/multipart-form",
-    upload.array("contents"),
-    async (request, response) => {
-      const contents = request.files;
-
-      const predictions = await Promise.all(
-        contents.map(async (content) => {
-          const tfContent = await tf.node.decodeImage(content.buffer, 3);
-          const prediction = await model.classify(tfContent);
-          tfContent.dispose();
-          return prediction;
-        })
-      );
-
-      return response.json({ predictions });
-    }
-  );
-
-  app.listen(3333);
+    return tf.tensor3d(values, [image.height, image.width, numChannels], 'int32')
 }
 
-main();
+app.post('/single/multipart-form', upload.single("content"), async (req, res) => {
+    if (!req.file) {
+        res.status(400).send("file is missing")
+    } else {
+        const image = await convert(req.file.buffer)
+        const prediction = await _model.classify(image)
+        image.dispose()
+        res.json({ prediction })
+    }
+})
+
+app.post("/multiple/multipart-form", upload.array("contents"), async (request, response) => {
+        const contents = request.files;
+        const predictions = await Promise.all(
+            contents.map(async (content) => {
+                const tfContent = await convert(req.file.buffer)
+                const prediction = await  _model.classify(tfContent)
+                tfContent.dispose()
+                return prediction
+            })
+        )
+        return response.json({ predictions })
+    }
+)
+
+const load_model = async () => {
+    _model = await nsfw.load()
+}
+
+// Keep the model in memory, make sure it's loaded only once
+load_model().then(() => app.listen(3333))
