a164766c6dfec6f43a337cbb950e35cfc2d7ff67
André Ribeiro <andresribeiroo@proton.me>
2023-01-28 14:12:43 -0300

    :sparkles:  add sharp

--- src/getPrediction.ts
+++ src/getPrediction.ts
@@ -1,12 +1,23 @@
 import * as tf from "@tensorflow/tfjs-node";
 import * as nsfwjs from "nsfwjs";
+import sharp from "sharp";
 
 tf.enableProdMode();
 
 const model = await nsfwjs.load("file://src/model/", { type: "graph" });
 
 export async function getPrediction(imageBuffer: Buffer) {
-	const tfImage = tf.node.decodeImage(imageBuffer, 3);
+	const jpeg = await sharp(imageBuffer)
+		.jpeg({ quality: 100 })
+		.resize({
+			width: 1080,
+			height: 1080,
+			fit: "cover",
+			withoutEnlargement: true,
+		})
+		.toBuffer();
+
+	const tfImage = tf.node.decodeImage(jpeg, 3);
 
 	const prediction = await model.classify(tfImage);
 
